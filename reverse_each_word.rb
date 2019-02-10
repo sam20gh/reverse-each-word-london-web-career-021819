f67af931f0e780b3dac9434e331d2b11553cb082
sam ghoreishi <sam20gh@gmail.com>
2019-02-10 22:58:10 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,3 +3,8 @@ def reverse_each_word(words)
   reversed_array = new_array.each {|x| x.reverse!}
   return reversed_array.join(" ")
 end
+
+
+def reverse_each_word(words)
+  words.split(" ").collect {|x| x.reverse!}.join(" ")
+end
